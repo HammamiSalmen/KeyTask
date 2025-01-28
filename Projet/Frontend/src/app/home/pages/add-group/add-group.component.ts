1351ffbfc683ff9cba54372f03db67c2e4e0e37d
Trabelsi-Fatma <fatmapandouche@gmail.com>
2025-01-28 10:52:45 +0100

    new update for join

--- Projet/Frontend/src/app/home/pages/add-group/add-group.component.ts
+++ Projet/Frontend/src/app/home/pages/add-group/add-group.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common'
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-group',
@@ -9,4 +11,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './add-group.component.css'
 })
 export class AddGroupComponent {
+  admin = {
+      grpname: '7amma',
+      password: '',
+    };
+  
+    constructor(private router: Router) {}
+  
+    onSubmit() {
+    
+      // Navigate to the home component after sign-up
+      this.router.navigate(['/add-task'], { state: { admin: this.admin } });
+    }
 }
